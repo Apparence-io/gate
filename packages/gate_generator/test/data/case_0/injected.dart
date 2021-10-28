1f0c918bfef3bc402cd71daea3f22e3a9514bcac
Gautier <gautier@apparence.io>
2021-10-28 23:11:49 +0200

    dart upgrade

--- packages/gate_generator/test/data/case_0/injected.dart
+++ packages/gate_generator/test/data/case_0/injected.dart
@@ -1,6 +1,9 @@
+import 'package:gate_generator/gate/gate_provider.dart';
 import 'package:gate/gate.dart';
 import 's1.dart';
 import 's2.dart';
+
+part 'injected.gate_inject.g.part';
 
 @Inject(children: [
   InjectedChild(S1, factoryName: 'build'),
