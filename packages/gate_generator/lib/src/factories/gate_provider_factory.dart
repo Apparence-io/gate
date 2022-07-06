e74f51d2836eaecdf5c0e6dfcd9c3bf92b8ad962
Gautier <gautier@apparence.io>
2022-07-06 14:37:33 +0200

    add ignore rules to generated code

--- packages/gate_generator/lib/src/factories/gate_provider_factory.dart
+++ packages/gate_generator/lib/src/factories/gate_provider_factory.dart
@@ -25,11 +25,13 @@ class GateProviderFactory {
 
     final provider = Class((b) => b
       ..docs.addAll([
-        '',
+        '// coverage:ignore-file ',
         '// ********************************',
-        '// Gate AppProvider generated file',
+        '// Gate generated file',
         '// Do not modify by hand',
-        '// ********************************'
+        '// ********************************',
+        '// ignore_for_file: type=lint',
+        '// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target'
       ])
       ..name = 'AppProvider'
       ..fields.add(Field((b) => b
